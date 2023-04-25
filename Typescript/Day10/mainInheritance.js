a413173c8e62e14da6d1ecfff275fe6f90780a3a
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-04-25 20:52:49 +0530

    Day10

---
+++ Typescript/Day10/mainInheritance.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var circle_1 = require("./circle");
+var rectangle_1 = require("./rectangle");
+var circlOBJ = new circle_1.Circle(2);
+circlOBJ.Myarea();
+circlOBJ.display();
+var rectObj = new rectangle_1.Rectangle(2, 3);
+rectObj.Myarea();
+rectObj.display();
---
+++ Typescript/Day10/mainInheritance.ts
@@ -0,0 +1,11 @@
+
+import {Circle} from './circle';
+import {Rectangle} from './rectangle';
+
+let circlOBJ=new Circle(2);
+circlOBJ.Myarea();
+circlOBJ.display();
+
+let rectObj=new Rectangle(2,3);
+rectObj.Myarea();
+rectObj.display();
