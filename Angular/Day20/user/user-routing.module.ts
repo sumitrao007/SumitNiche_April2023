1cd550806780cc5ebaa3078978375eda712aa28f
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-06-20 17:57:08 +0530

    Day20

---
+++ Angular/Day20/user/user-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListuserComponent } from './listuser/listuser.component';
+import { AdduserComponent } from './adduser/adduser.component';
+import { UpdateuserComponent } from './updateuser/updateuser.component';
+import { DeleteuserComponent } from './deleteuser/deleteuser.component';
+
+const routes: Routes = [
+  {
+    path:"",component:ListuserComponent
+  },
+  {
+    path:"add",component:AdduserComponent
+  },
+  {
+    path:"update",component:UpdateuserComponent
+  },
+  {
+    path:"delete",component:DeleteuserComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
